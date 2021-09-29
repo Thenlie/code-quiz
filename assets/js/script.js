b31af9ee42b3e4e33ee6d41d8ef9940f24063dec
Leithen <leithen113@gmail.com>
2021-09-29 17:14:25 -0600

    Working on a few bugs with the gameEnd screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,29 +12,29 @@ var timeLeft = 0; //set timer to 75
 timerEl.textContent = 'Time: ' + timeLeft;
 
 //set var for whether or not user got answer right
-var correctAns = document.createElement('div'); 
+var correctAns = document.createElement('div');
 correctAns.className = 'user-answer'
 correctAns.textContent = 'CORRECT!'
-var incorrectAns = document.createElement('div'); 
+var incorrectAns = document.createElement('div');
 incorrectAns.className = 'user-answer'
 incorrectAns.textContent = 'WRONG!'
 
 //create question objects
-var questionOne = {question: 'Commonly used data types do NOT include:', answerOne: 'Strings', answerTwo: 'Booleans', answerThree: 'Alerts', answerFour: 'Numbers', solution: 'question-btn-three'};
+var questionOne = { question: 'Commonly used data types do NOT include:', answerOne: 'Strings', answerTwo: 'Booleans', answerThree: 'Alerts', answerFour: 'Numbers', solution: 'question-btn-three' };
 
-var questionTwo = {question: 'The condition in an if/else statement is enclosed with:', answerOne: 'Quotes', answerTwo: 'Curly Brackets', answerThree: 'Parenthesis', answerFour: 'Square Brackets', solution: 'question-btn-two'};
+var questionTwo = { question: 'The condition in an if/else statement is enclosed with:', answerOne: 'Quotes', answerTwo: 'Curly Brackets', answerThree: 'Parenthesis', answerFour: 'Square Brackets', solution: 'question-btn-two' };
 
-var questionThree = {question: 'A useful tool used during debugging to print content to the debugger is:', answerOne: 'JavaScript', answerTwo: 'For Loops', answerThree: 'Terminal/Bash', answerFour: 'console.log', solution: 'question-btn-four'};
+var questionThree = { question: 'A useful tool used during debugging to print content to the debugger is:', answerOne: 'JavaScript', answerTwo: 'For Loops', answerThree: 'Terminal/Bash', answerFour: 'console.log', solution: 'question-btn-four' };
 
-var questionFour = {question: 'Arrays can be used to store:', answerOne: 'Numbers and Strings', answerTwo: 'Other Arrays', answerThree: 'Booleans', answerFour: 'All of the Above', solution: 'question-btn-four'};
+var questionFour = { question: 'Arrays can be used to store:', answerOne: 'Numbers and Strings', answerTwo: 'Other Arrays', answerThree: 'Booleans', answerFour: 'All of the Above', solution: 'question-btn-four' };
 
-var questionFive = {question: 'String values must be enclosed within ___ when being assigned to variables.', answerOne: 'Quotes', answerTwo: 'Curly Brackets', answerThree: 'Parenthesis', answerFour: 'Square Brackets', solution: 'question-btn-one'};
+var questionFive = { question: 'String values must be enclosed within ___ when being assigned to variables.', answerOne: 'Quotes', answerTwo: 'Curly Brackets', answerThree: 'Parenthesis', answerFour: 'Square Brackets', solution: 'question-btn-one' };
 
 //create question HTML elements
 var questionHead = document.createElement('h1'); //create h1 element
 questionHead.className = 'question-head';
-var questionOl = document.createElement('ol'); //create ordered list element
-questionOl.className = 'question-list'
+var questionDiv = document.createElement('ol'); //create ordered list element
+questionDiv.className = 'question-list'
 var questionBtnOne = document.createElement('button'); //create button element
 questionBtnOne.className = 'question-btn-one'
 var questionBtnTwo = document.createElement('button'); //create button element
@@ -44,28 +44,34 @@ questionBtnThree.className = 'question-btn-three'
 var questionBtnFour = document.createElement('button'); //create button element
 questionBtnFour.className = 'question-btn-four'
 
-var startGame = function() {
+//create highscore input element
+var userScore = document.createElement('input');
+userScore.className = 'user-score';
+userScore.type = 'text';
+userScore.placeholder = 'Enter initials';
+
+var startGame = function () {
     timeLeft = 75; //set timer to initial value
     mainHead.remove(); //removes initial main heading
     mainP.remove(); //removes initial main paragraph
     startBtnEl.remove(); //removes start button
 
-    timeInt = setInterval(function() { //declare global variable for timer
+    timeInt = setInterval(function () { //declare global variable for timer
         if (timeLeft > 0) {
             timerEl.textContent = 'Time: ' + timeLeft; //write timeLeft to the timer element
             timeLeft--; //decrement timer every interval
         } else {
             timerEl.textContent = ''; //remove timer from screen
             clearInterval(timeInt); //clear timer
-            stopGame(); 
+            stopGame();
         }
     }, 1000);
-    
+
     createQuestion();
 }
 
-var createQuestion = function() { //generates a question from the list
-    questionCount++; 
+var createQuestion = function () { //generates a question from the list
+    questionCount++;
     if (questionCount === 1) {
         questionNum = questionOne;
     } else if (questionCount === 2) {
@@ -76,36 +82,33 @@ var createQuestion = function() { //generates a question from the list
         questionNum = questionFour;
     } else if (questionCount === 5) {
         questionNum = questionFive;
-    } else {
-        stopGame();
-        return;
     }
 
     questionHead.textContent = questionNum.question;
-    pageContentEl.appendChild(questionHead); //add h1 to page (question)
+    pageContentEl.appendChild(questionHead);    //add h1 to page (question)
 
-    pageContentEl.appendChild(questionOl); //add ol to page (container)
+    pageContentEl.appendChild(questionDiv); //add ol to page (container)
 
     questionBtnOne.textContent = questionNum.answerOne;
-    questionOl.appendChild(questionBtnOne); //add button to ol (option 1)
+    questionDiv.appendChild(questionBtnOne); //add button to ol (option 1)
 
     questionBtnTwo.textContent = questionNum.answerTwo;
-    questionOl.appendChild(questionBtnTwo); //add button to ol (option 2)
+    questionDiv.appendChild(questionBtnTwo); //add button to ol (option 2)
 
     questionBtnThree.textContent = questionNum.answerThree;
-    questionOl.appendChild(questionBtnThree); //add button to ol (option 3)
+    questionDiv.appendChild(questionBtnThree); //add button to ol (option 3)
 
-    questionBtnFour.textContent = questionNum.answerFour; 
-    questionOl.appendChild(questionBtnFour); //add button to ol (option 4)
+    questionBtnFour.textContent = questionNum.answerFour;
+    questionDiv.appendChild(questionBtnFour); //add button to ol (option 4)
 
     var questionBtnEl = document.querySelector('.question-list');
     questionBtnEl.addEventListener('click', newQuestion); //listens for click on the question buttons
 }
 
-var newQuestion = function(event) {
+var newQuestion = function (event) {
     correctAns.remove();
     incorrectAns.remove();
-    
+
     var btnPressed = event.target; //define btnPressed as whichever button was clicked
     if (btnPressed.className === questionNum.solution && questionCount < 5) { //check if the button is the same as the solution   
         createQuestion();
@@ -114,7 +117,7 @@ var newQuestion = function(event) {
         timeLeft -= 10;
         createQuestion();
         pageContentEl.appendChild(incorrectAns);
-    } else if (btnPressed.className === questionNum.solution) {
+    } else if (btnPressed.className === questionNum.solution) { //check if it is the last question
         stopGame();
         return;
     } else {
@@ -124,12 +127,17 @@ var newQuestion = function(event) {
     }
 }
 
-function stopGame() {     //once the timer hits zero or all questions have been answered, run this function
-    questionHead.remove();
-    questionOl.remove();
+function stopGame() { //once the timer hits zero or all questions have been answered, run this function
     clearInterval(timeInt); //stop time
-    timerEl.textContent = 'Time: ' + timeLeft;
-    console.log('The game has ended! You score is: ' + timeLeft);
+    if (timeLeft >= 0) { //make sure time does not go negative
+        timerEl.textContent = 'Time: ' + timeLeft;
+    } else {
+        timeLeft = 0;
+        timerEl.textContent = 'Time: ' + timeLeft;
+    }
+    questionHead.textContent = 'All Done!';
+    questionDiv.textContent = 'Your final score is ' + timeLeft;
+    questionDiv.appendChild(userScore);
 }
 
 startBtnEl.addEventListener('click', startGame); //listens for click on start button, then calls function
